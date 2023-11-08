2d4c4b4ddf1be4c68f639d1097a943fa76905eae
anouar <tamagoult.anouar@gmail.com>
2023-11-08 11:16:04 +0100

    page index

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,26 +1,54 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+async function recupData() {
+
+
+    // Récupération des pièces depuis le fichier JSON
+    const reponse = await fetch('../data/photographers.json');
+    const fiches = await reponse.json();
+    console.log(fiches);
+    const sectionFiches = document.querySelector(".photographer_section"); 
+
+    for (let i = 0; i < fiches.photographers.length; i++) {
+        const carte = fiches.photographers[i];
+
+        // Création d’une balise dédiée à une fiche de Photographe (remplacement de "carte" par "div")
+        const ficheElement = document.createElement("div");
+
+        // Création des balises
+        const imageElement = document.createElement("img");
+        imageElement.src = carte.portrait;
+
+        const nomElement = document.createElement("h2");
+        nomElement.innerText = carte.name;
+
+        const locationElement = document.createElement("h5");
+        locationElement.innerText = carte.city + ", " + carte.country;
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = carte.tagline;
+        const priceElement = document.createElement("h6");
+        priceElement.innerText = carte.price + "€/jour ";
+
+        // On rattache la balise ficheElement à la section Fiches
+        sectionFiches.appendChild(ficheElement);
+
+        // On rattache l’image à ficheElement (la balise carte)
+        ficheElement.appendChild(imageElement);
+        ficheElement.appendChild(nomElement);
+        ficheElement.appendChild(locationElement);
+        ficheElement.appendChild(descriptionElement);
+        ficheElement.appendChild(priceElement);
+    }
+
+    // au choix d'une fiche de photographe
+    const ficheClick = document.querySelector(".photographer_section div");
+    ficheClick.addEventListener("click",()=>{
+        let message = prompt("merci");
+    })
+}
+recupData();
+
+
+/*
+async function getPhotographers() {
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
             photographers: [...photographers, ...photographers, ...photographers]})
@@ -42,5 +70,5 @@
         displayData(photographers);
     }
     
-    init();
-    
+    init();*/
+
